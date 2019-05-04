1dbc96a2e37b2eac10977ceb6f6a2396806a0f2c
Daryl110 <daryl.8220@gmail.com>
2019-05-04 10:27:45 -0500

    primer commit otra vez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,10 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { LoginComponent } from './components/users/login/login.component';
 import { RegistroComponent } from './components/users/registro/registro.component';
 import { HomeComponent } from './components/home/home.component';
 import { ConvocatoriaComponent } from './components/convocatoria/convocatoria.component';
-=======
-import { LoginComponent } from './componets/users/login/login.component';
-import { RegistroComponent } from './componets/users/registro/registro.component';
-import { HomeComponent } from './componets/home/home.component';
-import { ConvocatoriaComponent } from './componets/convocatoria/convocatoria.component';
-import { CitacionComponent } from './componets/citacion/citacion.component';
->>>>>>> 35bc8812ac612336e61c8148344fb2b9171bf551
 
 @NgModule({
   declarations: [
@@ -28,12 +20,7 @@ import { CitacionComponent } from './componets/citacion/citacion.component';
     LoginComponent,
     RegistroComponent,
     HomeComponent,
-<<<<<<< HEAD
     ConvocatoriaComponent
-=======
-    ConvocatoriaComponent,
-    CitacionComponent
->>>>>>> 35bc8812ac612336e61c8148344fb2b9171bf551
   ],
   imports: [
     BrowserModule,
