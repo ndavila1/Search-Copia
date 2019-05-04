103f591769ffd32889ab4310ec18709dafde9538
Daryl110 <daryl.8220@gmail.com>
2019-05-04 10:22:51 -0500

    primer commit x2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './componets/users/login/login.component';
-import { RegistroComponent } from './componets/users/registro/registro.component';
-import { HomeComponent } from './componets/home/home.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { RegistroComponent } from './components/users/registro/registro.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
